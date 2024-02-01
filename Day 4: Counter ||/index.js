1f2b3ca49583bd9fefd44bc7f124e2eccbae79df
Mastoora Turkmen <janmastoorajan@gmail.com>
2024-02-01 22:31:43 +0430

    Day 4: Counter ||

--- Day 4: Counter ||/index.js
+++ Day 4: Counter ||/index.js
@@ -3,7 +3,22 @@
  * @return { increment: Function, decrement: Function, reset: Function }
  */
 var createCounter = function(init) {
+   let count = init;
 
+   return {
+       increment: function() {
+           count ++;
+           return count
+       },
+       decrement: function(){
+           count -= 1;
+           return count;
+       },
+       reset: function() {
+           count = init;
+           return count
+       }
+   }
 };
 
 /**
