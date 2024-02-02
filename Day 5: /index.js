5f62229434d489ca07012c6da54dc4901d6fc2ea
Mastoora Turkmen <janmastoorajan@gmail.com>
2024-02-02 15:16:48 +0430

    Day 5: Apply Transform Over Each Element in Array

--- Day 5: /index.js
+++ Day 5: /index.js
@@ -5,3 +5,12 @@
  * @param {Function} fn
  * @return {number[]}
  */
+
+var map = function(arr, fn) {
+    let result = []
+    for(let i = 0; i < arr.length; i++) {
+        result.push(fn(arr[i], Number(i)))
+    }
+    return result
+
+};
