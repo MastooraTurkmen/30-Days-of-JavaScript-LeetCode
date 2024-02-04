ee5f32ab4c9629dc4a5cf8403badd9ede42db0f9
Mastoora Turkmen <janmastoorajan@gmail.com>
2024-02-04 08:23:00 +0430

    Day 6: Filter Elements from Array

--- Day 6: Filter Elements from Array/index.js
+++ Day 6: Filter Elements from Array/index.js
@@ -5,5 +5,12 @@
  */
 
 var filter = function(arr, fn) {
+    let filtered = [];
+    for(let i = 0; i < arr.length; i++) {
+        if(fn(arr[i], i)) {
+            filtered.push(arr[i])
+        }
+    }
 
+    return filtered;
 };
