c17d68070a07474fcf09dc3fce4dc39efc2b5249
Mastoora Turkmen <janmastoorajan@gmail.com>
2024-02-05 21:55:14 +0430

    Day 8: Function Compusition

--- Day 8: Funtion Computsition/index.js
+++ Day 8: Funtion Computsition/index.js
@@ -1,1 +1,16 @@
-// 
+// Day 8: Function Compusition
+
+/**
+ * @param {Function[]} functions
+ * @return {Function}
+ */
+var compose = function(functions) {
+    
+    return function(x) {
+        let result = x;
+        for(let i = functions.length - 1 ; i >= 0; i--){
+            result = functions[i](result)
+        }
+        return result;
+    }
+};
