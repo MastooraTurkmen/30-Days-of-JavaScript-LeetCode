8178ee2890e144cbd347978287ecb745ff624d64
Mastoora Turkmen <janmastoorajan@gmail.com>
2024-02-06 09:11:56 +0430

    Day 9: Return Length of Arguments Passed

--- Day 9: Return Length of Arguments Passed/index.js
+++ Day 9: Return Length of Arguments Passed/index.js
@@ -1,3 +1,13 @@
 // Return Length of Arguments Passed
 
+/**
+ * @param {...(null|boolean|number|string|Array|Object)} args
+ * @return {number}
+ */
+var argumentsLength = function(...args) {
+    return args.length
+};
 
+/**
+ * argumentsLength(1, 2, 3); // 3
+ */
