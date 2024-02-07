c49eed05128f93c32be6508dfbdaacac50b4b16d
MastooraTurkmen <janmastoorajan@gmail.com>
2024-02-07 22:37:00 +0430

    Day 10: Allow One Function Call

--- Day 10: Allow One Function Call/index.js
+++ Day 10: Allow One Function Call/index.js
@@ -5,3 +5,12 @@
  * @param {Function} fn
  * @return {Function}
  */
+
+
+/**
+ * let fn = (a,b,c) => (a + b + c)
+ * let onceFn = once(fn)
+ *
+ * onceFn(1,2,3); // 6
+ * onceFn(2,3,6); // returns undefined without calling fn
+ */
