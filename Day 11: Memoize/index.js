29a6781d428c237ac85a35ee954d0bfefc8ec478
MastooraTurkmen <janmastoorajan@gmail.com>
2024-02-17 21:48:45 +0430

    Day 11: Memoize

--- Day 11: Memoize/index.js
+++ Day 11: Memoize/index.js
@@ -3,6 +3,24 @@
 /**
  * @param {Function} fn
  * @return {Function}
+*/
+
+
+function memoize(fn) {
+    const cache = {};
+
+    return function (...args) {
+        const key = JSON.stringify(args);
+        if (cache.hasOwnProperty(key)) {
+            return cache[key];
+        }
+
+        const result = fn.apply(null, args);
+        cache[key] = result;
+
+        return result;
+    }
+}
 
 
 /** 
