31bdf89b2db35c39e5090cb655a833d85fea7a56
MastooraTurkmen <janmastoorajan@gmail.com>
2024-02-17 21:48:27 +0430

    Day 11: Memoize

--- Day 11: Memoize/index.js
+++ Day 11: Memoize/index.js
@@ -1,2 +1,17 @@
 // Memoize
 
+/**
+ * @param {Function} fn
+ * @return {Function}
+
+
+/** 
+ * let callCount = 0;
+ * const memoizedFn = memoize(function (a, b) {
+ *	 callCount += 1;
+ *   return a + b;
+ * })
+ * memoizedFn(2, 3) // 5
+ * memoizedFn(2, 3) // 5
+ * console.log(callCount) // 1 
+ */
