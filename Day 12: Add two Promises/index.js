c3af9caff4f6523b48eedbd5c6a01b9fb8cb26b9
MastooraTurkmen <janmastoorajan@gmail.com>
2024-02-17 22:00:35 +0430

    Day 12: Add two Promises

--- Day 12: Add two Promises/index.js
+++ Day 12: Add two Promises/index.js
@@ -1,2 +1,13 @@
 // Add two Promises
 
+/**
+ * @param {Promise} promise1
+ * @param {Promise} promise2
+ * @return {Promise}
+ */
+
+
+/**
+ * addTwoPromises(Promise.resolve(2), Promise.resolve(2))
+ *   .then(console.log); // 4
+ */
