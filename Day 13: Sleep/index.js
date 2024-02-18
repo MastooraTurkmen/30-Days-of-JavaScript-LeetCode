24f0caa3aa8e2a5b581ec21a841080e2ac782ed0
MastooraTurkmen <janmastoorajan@gmail.com>
2024-02-18 16:25:49 +0430

    Day 13: Sleep

--- Day 13: Sleep/index.js
+++ Day 13: Sleep/index.js
@@ -5,6 +5,15 @@
  * @return {Promise}
  */
 
+async function sleep(millis) {
+    return new Promise((res) => {
+        setTimeout(() => {
+            res(millis)
+        }, millis)
+    })
+}
+
+
 /** 
  * let t = Date.now()
  * sleep(100).then(() => console.log(Date.now() - t)) // 100
